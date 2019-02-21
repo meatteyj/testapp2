4587fb59891256d56c86347042ef45c99d539c4b
meatteyj <meatteyj@wit.edu>
2019-02-21 01:10:30 +0000

    WTTTTFFF

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //require jquery
-//= require jquery_ujs
+//= require jquery
 //= require turbolinks
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require_tree .
