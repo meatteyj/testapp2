adf90c8f8a4174614ea84f7ba6e2f2fc683f1ff4
meatteyj <alabamayamma@gmail.com>
2019-02-25 00:39:27 +0000

    uploading state of working webpage

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190224174642) do
+ActiveRecord::Schema.define(version: 20190224210914) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
